df37a955b92889e26bbda6b33b8296f6d8a721d0
NikeBoy128 <haroldmosquera739@gmail.com>
2024-05-24 13:09:15 -0500

    Update database configuration to use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mariadb',
-      host: '10.10.10.3',
-      port: 3306,
-      username: 'harold23',
-      password: 'admin',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [`${__dirname}/users/entities/**/*.entity.{ts,js}`],
       synchronize: false,
     }),
